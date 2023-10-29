66010335a27233beeea95fe390f3e3de44ddf03c
EthMoab <ethmoab@gmail.com>
2023-10-29 11:36:17 -0700

    start

---
+++ hardhat.config.js
@@ -0,0 +1,39 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+
+require('@openzeppelin/hardhat-upgrades');
+require("@nomicfoundation/hardhat-chai-matchers")
+
+const { PRIVATEKEY, APIKEY} = require("./pvkey.js");
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled : true,
+            runs: 2048,
+          }
+        }
+      },
+    ]
+  },
+
+
+  networks : {
+    
+    base_mainnet: {
+      url: 'https://mainnet.base.org',
+      chainId : 8453,
+      accounts : PRIVATEKEY
+    },
+
+    hardhat: {
+      forking: {
+        url : 'https://mainnet.base.org',
+        chainId : 8453
+      }
+    },
+  },
+
+};
