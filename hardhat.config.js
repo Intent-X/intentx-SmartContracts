3eb560918923fb60e8664aeedeae5839aa8cc559
EthMoab <ethmoab@gmail.com>
2024-04-04 00:22:58 +0400

    minigame

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,12 +3,12 @@
 require("@nomicfoundation/hardhat-chai-matchers")
 require("@openzeppelin/hardhat-upgrades");
 
-const { PRIVATEKEY, APIKEY} = require("./pvkey.js");
+const { INTENTXPRIVATEKEY, COREPRIVATEKEY, APIKEY} = require("./pvkey.js");
 module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.19",
+        version: "0.8.18",
         settings: {
           optimizer: {
             enabled : true,
@@ -19,13 +19,19 @@ module.exports = {
     ]
   },
 
-  defaultNetwork: "base_mainnet",
+  defaultNetwork: "blast_mainnet",
   networks : {
     
     base_mainnet: {
       url: 'https://mainnet.base.org',
       chainId : 8453,
-      accounts : PRIVATEKEY
+      accounts : INTENTXPRIVATEKEY
+    },
+
+    blast_mainnet: {
+      url: 'https://blast.blockpi.network/v1/rpc/public	',
+      chainId : 81457,
+      accounts : INTENTXPRIVATEKEY
     },
 
     /*hardhat: {
