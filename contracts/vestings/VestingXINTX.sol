6a1a8d8c51335f82f79e2945815e53ab21764950
EthMoab <ethmoab@gmail.com>
2024-05-30 22:53:33 +0400

    vesting

--- contracts/vestings/VestingXINTX.sol
+++ contracts/vestings/VestingXINTX.sol
@@ -17,42 +17,29 @@ contract VestingXINTX is ReentrancyGuardUpgradeable, Ownable2StepUpgradeable {
 
     using SafeERC20 for IERC20;
 
-    uint constant public PRECISION = 10000;
+    uint constant public PRECISION = 100000;
     uint public START_VESTING;
     uint public VESTING_DURATION;
-    uint private totalAmountNeeded;
-    address public multisig;
-
-    bool public allowChange;
-    bool public allowOwnerChange;
-
-    bool public seeded;
+    uint public NO_CLAIM_DURATION;
     
     IERC20 public intx;
     IStakedINTX public xIntx;
-
-    
     
     mapping(address => uint) public claimableAmount;
     mapping(address => uint) public claimedAmount;
     
-    event AllocationSet(address indexed user, uint amount);
-    event AllocationChanged( address changedBy, address indexed _oldAddress, address indexed _newAddress);
+    event Seeded( address operator, address[] users, uint[] amount,  uint totalAmount);
+    event AllocationChanged( address changedBy, address indexed _oldAddress, address indexed _newAddress, uint amountClaimable, uint amountClaimed);
     event Claimed(address indexed user, uint amount, uint _tokenId);
-    event Seeded( address operator, uint amount);
 
     constructor () { _disableInitializers(); }
 
     function initialize(
             address _intx,
             address _xIntx,
-            address _multisig,
             uint _startVesting,
             uint _vestingDuration,
-            address[] calldata _receivers,
-            uint[] calldata _amounts,
-            bool _allowChange,
-            bool _allowOwnerChange
+            uint _noClaimDuration
         ) public initializer {
         
         __ReentrancyGuard_init();
@@ -60,66 +47,35 @@ contract VestingXINTX is ReentrancyGuardUpgradeable, Ownable2StepUpgradeable {
 
         require ( _intx != address(0), "Can't use 0x address");
         require ( _xIntx != address(0), "Can't use 0x address");
-        require ( _multisig != address(0), "Can't use 0x address");
 
         intx = IERC20(_intx);
         xIntx = IStakedINTX(_xIntx);
 
-        multisig = _multisig;
-
         START_VESTING = _startVesting;
         VESTING_DURATION = _vestingDuration;
+        NO_CLAIM_DURATION = _noClaimDuration;
 
+    }
 
+    function setAllocations ( address[] calldata _receivers, uint[] calldata _amounts) external onlyOwner {
+        uint _amountNeeded = 0;
         uint _len = _receivers.length;
-
         require(_len == _amounts.length);
 
         for (uint i = 0; i < _len; i++) {
             claimableAmount[_receivers[i]] += _amounts[i];
-            totalAmountNeeded += _amounts[i];
-            emit AllocationSet(_receivers[i], _amounts[i]);
+            _amountNeeded += _amounts[i];
         }
 
-        allowChange = _allowChange;
-        allowOwnerChange = _allowOwnerChange;
 
+        intx.safeTransferFrom(_msgSender(), address(this), _amountNeeded);
+        emit Seeded( _msgSender(), _receivers, _amounts, _amountNeeded );
     }
 
-    function deposit() external onlyOwner {
-        require(!seeded, "Vesting Contract Already seeded with initial amount.");
 
-        intx.safeTransferFrom(_msgSender(), address(this), totalAmountNeeded);
-        intx.approve(address(xIntx), type(uint256).max);
-
-        seeded = true;
-        
-        emit Seeded( _msgSender(), totalAmountNeeded );
-    }
-
-    function changeReceiver( address _newAddress) external {
-        require( allowChange, "This vesting contract doesn't allow you to change the receiver address" );
-        require( _newAddress != address(0), "New allocation receiver can't be the 0x0000... address");
-
-        address _oldAddress = _msgSender();
-        require( claimableAmount[_oldAddress] != 0, "You don't have any allocation.");
-        require( claimableAmount[_newAddress] == 0, "The new address already has a vesting amount.");
-        require( claimedAmount[_oldAddress] < claimableAmount[_oldAddress], "You have already claimed all your allocation." );
-        
-        claimableAmount[ _newAddress ] = claimableAmount[ _oldAddress ];
-        claimedAmount[_newAddress] = claimedAmount[_oldAddress];
-
-        claimableAmount[ _oldAddress ] = 0;
-        claimedAmount[_oldAddress] = 0;
-
-        emit AllocationChanged( _msgSender(), _oldAddress, _newAddress);
-    }
-
-    function changeReceiverOwner( address _oldAddress, address _newAddress ) external onlyOwner {
-        require( allowOwnerChange );
-
+    function changeReceiver( address _oldAddress, address _newAddress ) external onlyOwner {
         require( claimableAmount[_oldAddress] != 0, "Old address doesn't have any allocation.");
-        require( claimedAmount[_oldAddress] < claimableAmount[_oldAddress], "You have already claimed all your allocation." );
+        require( claimedAmount[_oldAddress] < claimableAmount[_oldAddress], "This user has already claimed all your allocation." );
         
         // _newAddress will be address(0) when we just want to delete this user allocation.
         if ( _newAddress != address(0) ) {
@@ -128,21 +84,20 @@ contract VestingXINTX is ReentrancyGuardUpgradeable, Ownable2StepUpgradeable {
             claimedAmount[_newAddress] = claimedAmount[_oldAddress];
         } else {
             uint amountLeft = claimableAmount[ _oldAddress ] - claimedAmount[_oldAddress];
-            intx.safeTransfer(multisig, amountLeft);
+            intx.safeTransfer( _msgSender(), amountLeft);
         }
 
         claimableAmount[ _oldAddress ] = 0;
         claimedAmount[_oldAddress] = 0;
 
-        emit AllocationChanged( _msgSender(), _oldAddress, _newAddress);
+        emit AllocationChanged( _msgSender(), _oldAddress, _newAddress, claimableAmount[ _newAddress ], claimedAmount[ _newAddress ]);
     }
 
     function claim() public nonReentrant {
-        require(claimableAmount[_msgSender()] != 0,"This address doesn't have any amount to claim.");
-        require(seeded, "Vesting Contract hasn't been seeded yet.");
+        require( claimableAmount[_msgSender()] != 0,"This address doesn't have any amount to claim.");
         require( claimedAmount[_msgSender()] < claimableAmount[_msgSender()], "You have already claimed all your allocation" );
 
-        if (block.timestamp > START_VESTING) {
+        if (block.timestamp > START_VESTING + NO_CLAIM_DURATION) {
 
             uint amount = claimableAmount[_msgSender()];
 
@@ -159,6 +114,40 @@ contract VestingXINTX is ReentrancyGuardUpgradeable, Ownable2StepUpgradeable {
                 uint _tokenId = xIntx.stakeFor(_msgSender(), amountToReceive);
                 emit Claimed( _msgSender(), amountToReceive, _tokenId);
             }
+        }
+    }
+
+    function claimableNow( address _user) public view returns (uint amountToReceive) {
+
+        if (block.timestamp > START_VESTING + NO_CLAIM_DURATION) {
+
+            uint amount = claimableAmount[_user ];
+
+            uint timeElapsed = block.timestamp - START_VESTING;
+
+            if ( timeElapsed > VESTING_DURATION) timeElapsed = VESTING_DURATION;
+            
+            uint percentToReceive = timeElapsed * PRECISION / VESTING_DURATION;
+                
+            amountToReceive = (amount * percentToReceive / PRECISION) - claimedAmount[_user ];
+
+        }
+    }
+
+    function claimableWithoutCliff( address _user) public view returns (uint amountToReceive) {
+
+        if ( block.timestamp > START_VESTING ) {
+
+            uint amount = claimableAmount[_user ];
+
+            uint timeElapsed = block.timestamp - START_VESTING;
+
+            if ( timeElapsed > VESTING_DURATION) timeElapsed = VESTING_DURATION;
+            
+            uint percentToReceive = timeElapsed * PRECISION / VESTING_DURATION;
+                
+            amountToReceive = (amount * percentToReceive / PRECISION) - claimedAmount[_user ];
+
         }
     }
 
